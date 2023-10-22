413f85776d776dccb1b87e404e3eab8a3c42cdc0
DV.Sang <vansangIT2k3@gmai.com>
2023-10-22 22:40:41 +0700

    first commit

---
+++ 7 MUST KNOW JavaScript Tips and Tricks.js
@@ -0,0 +1,8 @@
+// ! 1. Destructuring with Parameters
+
+// ! 2. Deep Copy with JSON
+// ! 3. Easy Defaults with OR
+// ! 4. Advanced Array Searching
+// ! 5. Remove Array Duplicates
+// ! 6. Self-Invoking Functions
+// ! 7. Array Copying with Spread
